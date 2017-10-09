17fe8fd09e788d34738d73d383ce87fe330d249d
Alexandr Chernyakov <alexandrchernyakov@yandex.ua>
2017-10-09 19:43:59 +0300

    functions in TS

---
+++ types of function.ts
@@ -0,0 +1,18 @@
+let value:number = 0;
+
+function getNumber():number{
+    return value;
+}
+
+console.log(getNumber());
+
+function setNumber(num:number){
+    return num;
+}
+
+console.log(setNumber(5));
+
+function log():void{
+    console.log(5);
+}
+
