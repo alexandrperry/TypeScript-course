1a082feafbdabfe1d1ee004eb049339132395196
Alexandr Chernyakov <alexandrchernyakov@yandex.ua>
2017-10-09 19:11:49 +0300

    basic types of TS

---
+++ basic types.js
@@ -0,0 +1,8 @@
+var str = 'Hello';
+var num = 0;
+var tr = true;
+var res = 0;
+var test = 0;
+var numArr = [0, 1, 2];
+var strArr = ['hi', 'hello'];
+var arr = [1, 'hi'];
---
+++ basic types.ts
@@ -0,0 +1,16 @@
+let str:string = 'Hello';
+
+let num:number = 0;
+
+let tr:boolean = true;
+
+let res:any = 0;
+
+let test:string | number = 0;
+
+let numArr:number[] = [0,1,2];
+
+let strArr:Array<string> = ['hi','hello'];
+
+let arr:[number,string] = [1,'hi'];
+
